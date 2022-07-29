6abdf770592a3400870e5bfbc6c0f74af9d6b33a
Uswatun251219931 <fajaruswatunchasanah@gmail.com>
2022-07-30 01:36:27 +0700

    push pertama

---
+++ Test Suites/checkout.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>checkout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>efafdbc1-0549-4414-b04c-4c5b533dba2e</testSuiteGuid>
+   <testCaseLink>
+      <guid>d6f311f7-f200-474d-9662-d1bdc34095be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/flashsale</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
